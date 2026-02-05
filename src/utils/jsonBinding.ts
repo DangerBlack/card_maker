36b1a476ec41baf4f0c484c6e49d2a89435ce61d
DangerBlack <daniele.baschieri@cubbit.io>
2026-02-05 22:24:40 +0100

    fix(app): handle array of entities

--- src/utils/jsonBinding.ts
+++ src/utils/jsonBinding.ts
@@ -1,7 +1,14 @@
 import type {ImageElement, TextElement } from "../models/Element"
 
 export function resolveText(el: TextElement, data: Record<string, unknown>) {
-  if (el.bind) return String(data[el.bind] ?? "")
+  if(el.bind) {
+    if(Array.isArray(data[el.bind]))
+    {
+      return (data[el.bind] as unknown[]).join(", ")
+    }
+
+    return String(data[el.bind] ?? "")
+  }
   return el.staticText ?? ""
 }
 
